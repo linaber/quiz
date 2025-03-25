1b6e0a4b0a5b70cd22f1616f102cbee2a3c43d38
linaber <lina.berezane@gmail.com>
2025-03-25 12:15:15 +0200

    fix(init): resolve session and api issues - add sessions table migration - add test api route - verify filament admin access

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +19,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+      //  $controllerNamespace = 'App\Http\Controllers\Api';
+
+//        Route::prefix('api') // Префикс для всех маршрутов
+//        ->middleware('api') // Применяем middleware для API
+//        ->namespace($controllerNamespace) // Указываем пространство имен для контроллеров
+//        ->group(base_path('routes/api.php')); // Загружаем маршруты из файла api.php
+
+        Route::prefix('api')
+        ->middleware('api')
+        ->group(base_path('routes/api.php'));
     }
 }
